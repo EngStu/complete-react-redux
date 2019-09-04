cfb9141612f8dd49c9547a75ab44e60170228a6e
EngStu <engstu2015@gmail.com>
2019-09-04 18:29:44 +0630

    25_finished

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Deserunt incididunt ad pariatur dolor enim id ea nisi ullamco officia. Amet veniam quis eiusmod ut. Consequat qui duis excepteur in consequat exercitation occaecat incididunt cillum. Eiusmod ex qui aliqua dolore nostrud cillum minim eu.</p>
+    </div>
+  )
+}
+
+export default Contact
