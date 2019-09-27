3e9e2c8c7b11009c9bd71cda760677ef4821dd2e
engstu <engstu2015@gmail.com>
2019-09-27 09:51:57 +0630

    26_finished

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>loreamEnim labore tempor ut est consectetur aliquip ex amet nulla adipisicing dolore. Et enim excepteur sunt in aliquip fugiat dolor amet. Quis officia ullamco ex sint consectetur nisi adipisicing aute non voluptate. Sint proident incididunt eu nisi occaecat. Minim culpa consequat Lorem proident dolore do ex proident. Qui officia sunt anim aute ex cupidatat quis.</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>loreamEnim labore tempor ut est consectetur aliquip ex amet nulla adipisicing dolore. Et enim excepteur sunt in aliquip fugiat dolor amet. Quis officia ullamco ex sint consectetur nisi adipisicing aute non voluptate. Sint proident incididunt eu nisi occaecat. Minim culpa consequat Lorem proident dolore do ex proident. Qui officia sunt anim aute ex cupidatat quis.</p>
+    </div>
+  )
+}
+
+export default Home
